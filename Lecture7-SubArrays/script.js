1603166d5df8ba936de2af11dd1e8d923b4cf42b
Mayuresh50 <patilmayuresh5711@gmail.com>
2025-08-29 13:29:13 +0530

    New Changes Commited

--- Lecture7-SubArrays/script.js
+++ Lecture7-SubArrays/script.js
@@ -106,3 +106,56 @@
 // let k = 0
 // findlargesSubArrayofK(arr, k);
 
+//Write a function to print all substrings of size = k;
+// function findSumEqualtoK(arr, k) {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         let sum = 0;
+//         let subarray = [];
+//         for (let j = i; j < arr.length; j++) {
+//             sum = sum + arr[j];
+//             subarray.push(arr[j]);
+//             if (subarray.length === k) {
+//                 console.log({ subarray });
+//                 count++;
+//                 break;
+//             }
+//         }
+//     }
+//     console.log({ count })
+// }
+// let arr = [1, 2, - 3, 0, 1, -1, 1];
+// let k = 3
+// findSumEqualtoK(arr, k);
+//TC : O(n*k)
+
+//Vaibhav Sir's Approach 
+function allSubArrayOfSizeX(arr, k) {
+    let n = arr.length;
+    let subArrOfSizeX = [];
+    for (let i = 0; i < n; i++) {
+        if (i + k > n) {
+            break;
+        }
+        let subarr = [];
+        // for (let j = i; j < n; j++) {
+        //     subarr.push(arr[j]);
+        //     if (subarr.length === k) {
+        //         break;
+        //     }
+        // }
+        // console.log({ subarr })
+
+        for (let j = i; j < i + k; j++) {
+            subarr.push(arr[j]);
+        }
+        subArrOfSizeX.push(subarr);
+    }
+    return subArrOfSizeX;
+}
+
+let arr = [1, 2, - 3, 0, 1, -1, 1];
+let k = 3
+allSubArrayOfSizeX(arr, k);
+
+
