2b274e8f233357eb341c873320533c5611321057
Mayuresh50 <patilmayuresh5711@gmail.com>
2025-09-08 13:55:07 +0530

    Commit save

--- Lecture12-TimeAndSpaceComplexity/Handson.js
+++ Lecture12-TimeAndSpaceComplexity/Handson.js
@@ -15,3 +15,11 @@
 //     }
 // }
 
+// let i = 0
+// let n = 10
+// while (i <= n) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+//     i += 2;   //Inccrement value of i by 2
+// }
