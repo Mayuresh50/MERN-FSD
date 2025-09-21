06d53fb02b71b7e36faa7714df775008fc8b5d9d
Mayuresh50 <patilmayuresh5711@gmail.com>
2025-09-21 21:28:02 +0530

    Commit save

--- Lecture19-Sorting/handson.js
+++ Lecture19-Sorting/handson.js
@@ -30,6 +30,22 @@
 // // let arr = [-1, -5, -2, 2, 0, 4];
 // // console.log(sortWithNegatives(arr));
 
+//Bubble Sort
+// function bubbleSort(arr) {
+//     let n = arr.length;
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 let temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+// let arr1 = [4, 5, 1, 3, 2];
+// console.log("Bubble Sort:", bubbleSort(arr1));
 
 // function selectionSort(arr) {
 //     for (let i = 0; i < arr.length - 1; i++) {
@@ -85,5 +101,6 @@
 // }
 // let str = "banana";
 // console.log(findSubstring(str));
+
 
 
