ed4d50812fa8e6f8787222d319bfe9a9035fce46
Mayuresh50 <patilmayuresh5711@gmail.com>
2025-09-22 13:21:07 +0530

    Commit save

--- Lecture19-Sorting/handson.js
+++ Lecture19-Sorting/handson.js
@@ -1,34 +1,32 @@
-// // function sortWithNegatives(arr) {
-// //     let minVal = Math.min(...arr);    //Min Value in the array
-// //     let maxVal = Math.max(...arr);    //Max value in the array
-
-// //     let neg = new Array(Math.abs(minVal) + 1).fill(0);   //Array for negative values
-// //     let pos = new Array(maxVal + 1).fill(0);    //Array for positive values
-
-// //     for (let num of arr) {
-// //         if (num < 0) {
-// //             neg[Math.abs(num)] = (neg[Math.abs(num)] || 0) + 1;
-// //         } else {
-// //             pos[num] = (pos[num] || 0) + 1;
-// //         }
-// //     }
-// //     let result = [];
-// //     for (let i = neg.length - 1; i > 0; i--) {
-// //         while (neg[i] > 0) {
-// //             result.push(-i);
-// //             neg[i]--;
-// //         }
-// //     }
-// //     for (let i = 0; i < pos.length; i++) {
-// //         while (pos[i] > 0) {
-// //             result.push(i);
-// //             pos[i]--;
-// //         }
-// //     }
-// //     return result;
-// // }
-// // let arr = [-1, -5, -2, 2, 0, 4];
-// // console.log(sortWithNegatives(arr));
+// function sortWithNegatives(arr) {
+//     let minVal = Math.min(...arr);    //Min Value in the array
+//     let maxVal = Math.max(...arr);    //Max value in the array
+//     let neg = new Array(Math.abs(minVal) + 1).fill(0);   //Array for negative values
+//     let pos = new Array(maxVal + 1).fill(0);    //Array for positive values
+//     for (let num of arr) {
+//         if (num < 0) {
+//             neg[Math.abs(num)] = (neg[Math.abs(num)] || 0) + 1;
+//         } else {
+//             pos[num] = (pos[num] || 0) + 1;
+//         }
+//     }
+//     let result = [];
+//     for (let i = neg.length - 1; i > 0; i--) {
+//         while (neg[i] > 0) {
+//             result.push(-i);
+//             neg[i]--;
+//         }
+//     }
+//     for (let i = 0; i < pos.length; i++) {
+//         while (pos[i] > 0) {
+//             result.push(i);
+//             pos[i]--;
+//         }
+//     }
+//     return result;
+// }
+// let arr = [-1, -5, -2, 2, 0, 4];
+// console.log(sortWithNegatives(arr));
 
 //Bubble Sort
 // function bubbleSort(arr) {
